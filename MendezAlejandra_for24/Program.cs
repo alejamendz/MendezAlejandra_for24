d601d6d4cfcbce2c71d461d7a2e4d09f24656cb9
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-02-24 19:31:41 -0600

    Uso de for y fin del programa

--- MendezAlejandra_for24/Program.cs
+++ MendezAlejandra_for24/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Pedir N notas y mostrar si el promedio es aprobado (>=61) o reprobado.");
+
+Console.Write("¿Cuántas notas?: ");
+int n = int.Parse(Console.ReadLine());
+double suma = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese nota: ");
+    suma += double.Parse(Console.ReadLine());
+}
+
+double promedio = suma / n;
+
+if (promedio >= 61)
+    Console.WriteLine("Aprobado");
+else
+    Console.WriteLine("Reprobado");
